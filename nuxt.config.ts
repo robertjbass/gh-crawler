a72dd18716a5158bcfa4b515d1fa6926e847711a
Bob Bass <bob@Bobs-MacBook-Air-M2.local>
2023-10-18 20:53:11 -0500

    added nuxt UI

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/ui"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute();
+const placeholderData = await(
+  await fetch("https://jsonplaceholder.typicode.com/todos")
+).json();
+const data = ref(placeholderData);
+</script>
+
+<template>
+  <UContainer class="my-8">
+    <UCard>
+      <code>
+        <pre>{{ data }}</pre>
+      </code>
+    </UCard>
+  </UContainer>
+</template>
